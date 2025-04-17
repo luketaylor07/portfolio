d84dfa7b098088f9bbc3ee6495792a961c9a1d1b
luketaylor45 <luketaylor612@gmail.com>
2025-04-17 18:16:19 +0100

    Corrected links temp

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
     const { theme, setTheme } = useTheme();
 
     return (
-        <header className="bg-white dark:bg-zinc-900 fixed top-0 left-0 z-50 w-full p-5 flex justify-between items-center shadow-md">
+        <header id="header" className="bg-white dark:bg-zinc-900 fixed top-0 left-0 z-50 w-full p-5 flex justify-between items-center shadow-md">
             <div className="absolute right-0 mx-5">
                 <button
                     onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
@@ -25,18 +25,12 @@ const Header = () => {
                 </button>
             </div>
             <div className="flex flex-col items-center mx-auto">
-                <Link href="/" className="text-2xl font-semibold transition-colors duration-300 hover:text-red-500 dark:hover:text-red-600">
+                <Link to="header" className="text-2xl font-semibold transition-colors duration-300 hover:text-red-500 dark:hover:text-red-600">
                     Luke Taylor
                 </Link>
                 <div className="flex gap-6 mt-2">
                     <Link to="skills" smooth duration={100} className="cursor-pointer transition-colors duration-300 hover:text-red-500 dark:hover:text-red-600">
                         Skills
-                    </Link>
-                    <Link to="projects" smooth duration={100} className="cursor-pointer transition-colors duration-300 hover:text-red-500 dark:hover:text-red-600">
-                        Projects
-                    </Link>
-                    <Link href="/contact" className="transition-colors duration-300 hover:text-red-500 dark:hover:text-red-600">
-                        Contact
                     </Link>
                 </div>
             </div>
